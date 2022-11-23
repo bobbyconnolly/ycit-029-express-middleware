77baf8485ece4ab05085ed18fb02eeabc6165d73
Bobby Connolly <connollythomas@hotmail.com>
2022-11-23 15:32:47 -0500

    add steps

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 const express = require("express");
 
+const COUPON_CODES = ["123", "456", "789"];
+
 // Instantiate the app
 const app = express();
 
 // Add static files middleware
 app.use(express.static("public"));
 
-// Body parser middleware
-app.use(express.json());
+// JSON body parser middleware
+// app.use(express.json()); // I'm commenting this out because we don't need it for this example
 
 // Middleware to extract the form data from the request body
-app.use(express.urlencoded({ extended: true }));
+app.use(
+    express.urlencoded({
+        extended: true,
+        type: "application/x-www-form-urlencoded",
+    })
+);
 
 // Begin listening for requests
 app.listen(3000, () => {
